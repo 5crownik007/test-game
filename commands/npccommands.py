77b4fa9c979b2147417852fb2af20000c6551e03
5crownik007 <nikolapavkovic4@gmail.com>
2021-06-22 20:56:05 +1000

    Ingame conversation construction
    convotopic command is now capable of building full conversation trees, as well as expanded functionality for listing, viewing, adding, removing and editing topics stored on an NPC.

--- commands/npccommands.py
+++ commands/npccommands.py
@@ -2,7 +2,6 @@ from commands.command import Command
 from evennia import CmdSet, utils
 from evennia import syscmdkeys, default_cmds
 from evennia.utils import dedent
-from evennia.commands.default.building import ObjManipCommand
 from django.conf import settings
 _SEARCH_AT_RESULT = utils.object_from_module(settings.SEARCH_AT_RESULT)
 
@@ -27,82 +26,152 @@ class CmdConverse(Command):
         else:
             target.receive_converse(self.caller)
 
-# class CmdConvoTopic(ObjManipCommand):
-#     """
-#     View, add, remove and edit conversation topics on an NPC.
-#     Usage:
-#       convotopic <obj>
-#       convotopic <obj>/<key>
-#       convotopic <obj> = <dict> 
-#       convotopic/remove <obj>/<key>
-#       convotopic/edit <obj>/<key> = <dict>
-#     First form lists all topics on an object
-#     Second form views a specific topic on an object
-#     Third form adds a topic to an object
-#     Fourth form removes a topic from an object
-#     Fifth edits a topic on an object
-
-#     Remember, a conversation topic is always a dictionary with the following keywords:
-#         key(str): evmenu key for the option in the startconvo node
-#         desc(str): evmenu description shown with the option in startconvo node
-#         goto(str or callable or tuple(callable, kwargs)): node or goto-callable for the option in the startconvo node
-#         topic_text(str): text shown in the topicconvo node. irrelevant if not going to that node
-#         locktag(str or None): key of the tag required for this topic to be displayed. If set to None, no tag is required.
-#     """
-#     key = "convotopic"
-#     locks = "cmd:perm(builder)"
-#     help_category = "Building"
-
-#     def 
-
 class CmdConvoTopic(default_cmds.MuxCommand):
     """
-    Placeholder command to ADD conversation topics only
+    List, view, remove, add and edit conversation topics on an NPC.
     Usage:
-       convotopic <obj> = <dict>
+      convotopic <obj>
+      convotopic <obj>/<key>
+      convotopic/remove <obj>/<key>
+      convotopic <obj> = <dict> 
+      convotopic/edit <obj>/<key> = <dict>
+      convotopic/edit <obj>/<key>/<keyword> = <arg>
+    First form lists all topics on <obj>,
+    Second form views <key> on <obj>,
+    Third form removes <key> from <obj>,
+    Fourth form adds <dict> to <obj>,
+    Fifth form replaces <key> on <obj> with <dict>,
+    Sixth form replaces <keyword> on <key> on <obj> with <arg>
+
+    Remember, a conversation topic is always a dictionary with the following keywords:
+        key(str): evmenu key for the option in the startconvo node
+        desc(str): evmenu description shown with the option in startconvo node
+        goto(str or callable or tuple(callable, **kwargs)): node or goto-callable for the option in the startconvo node
+        topic_text(str): text shown in the topicconvo node. irrelevant if not going to that node
+        locktag(str or list[**str] None): key or keys of the tag required for this topic to be displayed. If set to None, no tag is required.
+        topictag(str or None): key of the tag given when on this this topic node. If set to None, no tag is given.
+    Here's an easy template you can paste into your input line:
+    {'key': ,'desc': ,'goto':,'topic_text': ,'locktag': ,'topictag': }
+
+    You can also add additional keywords beyond those above, and they will be passed in properly.
     """
     key = "convotopic"
     locks = "cmd:perm(builder)"
     help_category = "Building"
+    switch_options = ('remove','edit')
+
+    def checkusage(self):
+        #Checks which usage of the command is being sent to us and sets our self.usage variable
+        # accordingly.
+        if self.args:
+            if self.switches:
+                self.usage = self.switches[0]
+            elif self.rhs:
+                self.usage = 'add'
+            elif len(self.lhs.split('/')) > 1:
+                self.usage = 'view'
+            else:
+                self.usage = 'list'
+
+    def parse(self):
+        super().parse()
+        self.checkusage()          
+
     def func(self):
-        if not self.args or not self.rhs:
-            self.caller.msg("|rUsage: convotopic <obj> = <dict>|n")
-            return
-        try:
-            received = eval(self.rhs)
-        except:
-            received = self.rhs
-        if not isinstance(received, dict):
-            self.caller.msg("|rInput topic was not a dictionary.|n")
-            return
-        elif "key" or "desc" or "goto" or "topic_text" or "locktag" not in received:
+        if not self.args:
+            #If we don't have args, go back to the user and ask for some.
             self.caller.msg(dedent(
                 """
-                |rInput topic dictionary was missing one or more keywords.|n
-                |wRequired Keywords:|n
-                |wkey(str):|n evmenu key for the option in the startconvo node
-                |wdesc(str):|n evmenu description shown with the option in startconvo node
-                |wgoto(str or callable or tuple(callable, kwargs)):|n node or goto-callable for the option in the startconvo node
-                |wtopic_text(str):|n text shown in the topicconvo node. irrelevant if not going to that node
-                |wlocktag(str or None):|n key of the tag required for this topic to be displayed. If set to None, no tag is required.
+                |wUsage:|n
+                convotopic <obj>
+                convotopic <obj>/<key>
+                convotopic/remove <obj>/<key>
+                convotopic <obj> = <dict> 
+                convotopic/edit <obj>/<key> = <dict>
+                convotopic/edit <obj>/<key>/<keyword> = <arg>
                 """
             ))
             return
-        else:
-            target = self.caller.search(self.lhs,candidates=self.caller.location.contents,use_nicks=True,quiet=True)
-            if len(target) != 1:
-                _SEARCH_AT_RESULT(target,self.caller,self.args)
-                return
-            else:
-                target = target[0]
-        
-        if not hasattr(target, "add_convo_topic"):
-            self.caller.msg("You cannot set conversation topics on %s." %target.key)
+        target = self.caller.search(self.lhs.split('/')[0],candidates=self.caller.location.contents,use_nicks=True,quiet=True)
+        if len(target) != 1:
+            #Can't find the target, this is invalid. Search error handling will take it from here.
+            _SEARCH_AT_RESULT(target,self.caller,self.args)
             return
-        else:
-            target.add_convo_topic(received)
-            self.caller.msg("Topic added to %s: %s" %(self.lhs, self.rhs["key"]))
-        
+        #Found a target. Does this thing even have the ability to handle conversation topics? Let's check.
+        target = target[0]
+        if not hasattr(target, "add_convo_topic"):
+            #Technically, add_convo_topic is only one of the functions we use to interact with the
+            # conversations on NPCs, but if it's absent, then the other ones are likely absent as well.
+            self.caller.msg("%s has no handler for conversations." %target.key)
+            return
+        if self.usage == 'list':
+            #For list, all that is needed is a valid target, which we've just confirmed. We can call the
+            # relevant function now.
+            target.list_convo_topic(self.caller)
+            return
+        if self.usage == 'add' or self.usage == 'edit':
+            #For add and edit, we'll need a valid dictionary or keyword as one of the arguments. Let's
+            # test to see if we received either.
+            if not self.rhs:
+                self.caller.msg(dedent(
+                    """
+                    |wUsage:|n
+                    convotopic <obj>
+                    convotopic <obj>/<key>
+                    convotopic/remove <obj>/<key>
+                    convotopic <obj> = <dict> 
+                    convotopic/edit <obj>/<key> = <dict>
+                    convotopic/edit <obj>/<key>/<keyword> = <arg>
+                    """
+                ))
+                return
+            if len(self.lhs.split('/')) < 3:
+                try:
+                    received = eval(self.rhs)
+                except:
+                    received = self.rhs
+                if not isinstance(received, dict):
+                    self.caller.msg("Received input was not a dictionary.")
+                    return
+                else:
+                    keywords = ["key","desc","goto","topic_text","locktag","topictag"]
+                    for keyword in keywords:
+                        if keyword not in received:
+                            self.caller.msg(dedent(
+                                """
+                                |rInput topic dictionary was missing one or more keywords.|n
+                                |wRequired Keywords:|n
+                                |wkey(str):|n evmenu key for the option in the startconvo node
+                                |wdesc(str):|n evmenu description shown with the option in startconvo node
+                                |wgoto(str or str(callable) or tuple(str(callable), **kwargs)):|n node or goto-callable for the option. Callables must he put into strings for proper function.
+                                |wtopic_text(str):|n text shown in the topicconvo node. irrelevant if not going to that node
+                                |wlocktag(str or list[**str] None):|n key or keys of the tag required for this topic to be displayed. If set to None, no tag is required.
+                                |wtopictag(str or None):|n key of the tag given when on this this topic node. If set to None, no tag is given.
+                                """
+                            ))
+                            return
+                if self.usage == 'add':
+                    #Since we have a valid target and valid dictionary, we can add.
+                    target.add_convo_topic(self.caller,received)
+                    return
+            else:
+                received = [self.lhs.split('/')[2],self.rhs]
+
+        if self.usage == 'view' or self.usage == 'remove' or self.usage == 'edit':
+            #For cases where a valid key is necessary, let's see if it's there.
+            if not len(self.lhs.split('/')) > 1:
+                self.caller.msg("Key needed for %s."%self.usage)
+            else:
+                topic_key = self.lhs.split('/')[1]
+                if self.usage == 'view':
+                    #With a valid target and key, we can view and remove.
+                    target.view_convo_topic(self.caller,topic_key)
+                elif self.usage == 'remove':
+                    target.remove_convo_topic(self.caller,topic_key)
+                else:
+                    #With a valid target, key and dictionary/keyword+arg, we can edit.
+                    target.edit_convo_topic(self.caller,topic_key,received)
+
 class PC2NPCCmdSet(CmdSet):
     key = "pc2npc_cmdset"
     priority = 1
--- typeclasses/npcs.py
+++ typeclasses/npcs.py
@@ -14,12 +14,104 @@ class NonPlayerCharacter(Character):
         #  topic_text(str): text shown in the topicconvo node. irrelevant if not going to that node
         #  locktag(str or None): key of the tag required for this topic to be displayed. If set to None, no tag is required.
         self.db.convo_topics = [
-            {"key":"Example","desc":"This is an example topic.","goto":"topic","topic_text":"This is the text of the topic.","locktag":None}
+            {"key":"Example","desc":"This is an example topic.","goto":"topic","topic_text":"This is the text of the topic.","locktag":None,"topictag":None}
         ]
 
-    def add_convo_topic(self,new_topic):
-        #adds a conversation topic to the list
+    def add_convo_topic(self,caller,new_topic):
+        #Adds a conversation topic to the database.
+        for topic in self.db.convo_topics:
+            if topic["key"].lower() == new_topic["key"].lower():
+                #Trying to avoid duplicates if possible
+                caller.msg("Topic with key %s already exists."%new_topic["key"])
+                return
         self.db.convo_topics.append(new_topic)
+        caller.msg("Topic with key %s successfully added for %s."%(new_topic["key"],self.key))
+        
+    def list_convo_topic(self,caller):
+        #Messages a list of all topics to the caller.
+        topic_list = []
+        for topic in self.db.convo_topics:
+            topic_list.append(str(topic))
+        return_str = "\n".join(topic_list)
+        return_str = ''.join(["List of topics for %s.\n"%self.key,return_str,"\n%s topics found."%len(topic_list)])
+        caller.msg(return_str)
+
+    def view_convo_topic(self,caller,topic_key):
+        #Messages a specified topic to the caller.
+        view_topic = {}
+        view_topic_list = []
+        for topic in self.db.convo_topics:
+            if topic["key"].lower() == topic_key.lower():
+                view_topic = topic
+        if view_topic:
+            for keyword in view_topic:
+                    line = "|w%s|n: %s"%(keyword,view_topic[keyword])
+                    view_topic_list.append(line)
+            return_str = "\n".join(view_topic_list)
+            return_str = ''.join(["Found topic with key |w%s|n for %s.\n"%(topic_key,self.key),return_str])
+            caller.msg(return_str)
+        else:
+            caller.msg("Topic with key %s not found for %s"%(topic_key,self.key))
+
+    def remove_convo_topic(self,caller,topic_key):
+        #Removes a specified topic.
+        topic_found = False
+        for topic in self.db.convo_topics:
+            if topic["key"].lower() == topic_key.lower():
+                topic_found = True
+                self.db.convo_topics.remove(topic)
+                caller.msg("Topic %s successfully removed from %s."%(topic_key,self.key))
+        if not topic_found:
+            caller.msg("Topic with key %s not found for %s"%(topic_key,self.key))
+
+    def edit_convo_topic(self,caller,topic_key,received):
+        #Finds a specified topic and then replaces it with a new one OR finds a specified keyword on a
+        # topic and replaces it with a new one.
+        topic_list = self.db.convo_topics
+        old_topic_index = False
+        new_topic_key = False
+        new_topic_indices = []
+        if isinstance(received, dict):
+            new_topic_key = received["key"].lower()
+        elif received[0] == 'key':
+            new_topic_key == received[1].lower()
+        if new_topic_key:
+            for topic_index in range(len(topic_list)):
+                #Get indices which match our old topic key and our new topic key
+                if topic_list[topic_index]["key"].lower() == topic_key.lower():
+                    old_topic_index = topic_index
+                elif topic_list[topic_index]["key"].lower() == new_topic_key:
+                    new_topic_indices.append(topic_index)
+            if not old_topic_index:
+                caller.msg("Topic with key %s not found for %s"%(topic_key,self.key))
+                return
+            elif len(new_topic_indices) != 0:
+                if old_topic_index == new_topic_indices[0]:
+                    if isinstance(received, dict):
+                        topic_list[old_topic_index] = received
+                    else:
+                        topic_list[old_topic_index]["key"] = received[1]
+                else:
+                    caller.msg("Topic with key %s is already assigned for %s."%(new_topic_key,self.key))
+                    return            
+            else:
+                if isinstance(received, dict):
+                    topic_list[old_topic_index] = received
+                else:
+                    topic_list[old_topic_index]["key"] = received[1]
+        else:
+            topic_found = False
+            for topic in topic_list:
+                if topic["key"].lower() == topic_key.lower():
+                    topic[received[0]] = received[1]
+                    topic_found = True
+            if not topic_found:
+                caller.msg("Topic with key %s not found for %s"%(topic_key,self.key))
+        self.db.convo_topics = topic_list
+        if isinstance(received,dict):
+            caller.msg("Topic with key %s successfully edited for %s."%(topic_key,self.key))
+        else:
+            caller.msg("Topic with key %s successfully had keyword %s edited for %s."%(topic_key,received[0],self.key))
 
     def receive_converse(self, caller):
         #Called when someone converses with us, initiates the conversation menu.
@@ -30,26 +122,63 @@ class NonPlayerCharacter(Character):
             }
         evmenu.EvMenu(caller, convo_tree,startnode_input=('',{"greeted":False}))
 
-    def generate_options(self, caller):
+    def generate_options(self, caller, mode=0):
+        #when mode = 0
         #needs to return a list of dictionaries like so:
         # convo_options = [
-        #     {"key":"Greeting","desc":"Say hello to %s."% self.key,"goto":self.replygreeting},
-        #     {"key":"Topic1","desc":"Topic1desc","goto":"topic1goto"},
+        #     {"key":"Greeting","desc":"Say hello to %s."% self.key,"goto":self.replychoice},
+        #     {"key":"Topic1","desc":"Topic1desc","goto":"Topic1goto"},
         #     {"key":"Topic2","desc":"Topic2desc","goto":"Topic2goto"},
         #     {"key":"Topic3",...
         #     {"key":"Farewell","desc":"End the conversation.","goto":"end"}
         #     ]
+        #when mode = 1
+        #needs to return a list of dictionaries like so:
+        # convo_options = [
+        #     {"key":"Back","desc":"Select another conversation topic.","goto":self.replychoice},
+        #     {"key":"Topic1Sub1","Desc":"Subtopic 1 of Topic 1 ","goto":"Topic1Sub1goto",
+        #     {"key":"Topic1Sub2","Desc":"Subtopic 2 of Topic 1 ","goto":"Topic1Sub2goto",
+        #     {"key":"Topic1Sub3",...
         convo_options = []
-        convo_options.append({"key":"Greeting","desc":"Say hello to %s."% self.key,"goto":self.replygreeting})
-        if len(self.db.convo_topics) > 0:
-            for topic in self.db.convo_topics:
-                if not topic["locktag"] or caller.tags.get(topic["locktag"]):
-                    convo_options.append({"key":topic["key"],"desc":topic["desc"],"goto":topic["goto"]})
-        convo_options.append({"key":"Farewell","desc":"End the conversation.","goto":"end"})
+        if mode == 0:
+            convo_options.append({"key":"Greeting","desc":"Say hello to %s."% self.key,"goto":self.replychoice})
+            if len(self.db.convo_topics) > 0:
+                for topic in self.db.convo_topics:
+                    if not topic["locktag"]:
+                        convo_options.append({"key":topic["key"],"desc":topic["desc"],"goto":topic["goto"]})
+                    else:
+                        has_locktags = True
+                        locktags = topic["locktag"]
+                        if not isinstance(locktags, list):
+                            locktags = [locktags]
+                        for locktag in locktags:
+                            if locktag not in caller.tags.all():
+                                has_locktags = False
+                        if has_locktags:
+                            convo_options.append({"key":topic["key"],"desc":topic["desc"],"goto":topic["goto"]})
+            convo_options.append({"key":"Farewell","desc":"End the conversation.","goto":"end"})
+        else: 
+            convo_options.append({"key":"Back","desc":"Select another conversation topic.","goto":self.replychoice})
+            if len(self.db.convo_topics) > 0:
+                for topic in self.db.convo_topics:
+                    if topic["locktag"]:
+                        has_locktags = True
+                        locktags = topic["locktag"]
+                        if not isinstance(locktags, list):
+                            locktags = [locktags]
+                        for locktag in locktags:
+                            if locktag not in caller.tags.all():
+                                has_locktags = False
+                        if has_locktags:
+                            try:
+                                topic_goto = eval(topic["goto"])
+                            except:
+                                topic_goto = topic["goto"]
+                            convo_options.append({"key":topic["key"],"desc":topic["desc"],"goto":topic_goto})
         return convo_options
 
     def startconvo(self, caller, raw_string, **kwargs):
-        #This node is the "core" node for the whole conversation.
+        #This node is the "start" node for the whole conversation.
         if not kwargs["greeted"]:
             if self.db.greeting:
                 text = self.db.greeting %{"callername":caller.key,"ourname":self.key}                
@@ -63,19 +192,25 @@ class NonPlayerCharacter(Character):
         options = self.generate_options(caller)
         return text, options
 
-    def replygreeting(self, caller, raw_string, **kwargs):
-        #This callable will return us to the startconvo node, but with greeted as True
-        return "start", {"greeted":True}
+    def replychoice(self, caller, raw_string, **kwargs):
+        #This function very simply facilitates branching conversation construction by either sending the
+        # menu back to the "start" node, or re-running the "topic" node with new kwargs
+        #
+        if raw_string.lower() == 'back' or raw_string.lower() == 'greeting':
+            return "start", {"greeted":True}
+        else:
+            caller.tags.clear(category="%s_convo"%self.key)
+            return None, kwargs
 
     def topicconvo(self, caller, raw_string, **kwargs):
-        our_topic = {}
+        current_topic = {}
         for topic in self.db.convo_topics:
             if topic["key"].lower() == raw_string.lower():
-                our_topic = topic
-        text = our_topic["topic_text"]
-        options = [
-            {"key":"Back","desc":"Select another conversation topic.","goto":self.replygreeting}
-        ]
+                current_topic = topic
+        if current_topic["topictag"]:
+            caller.tags.add(current_topic["topictag"],category="%s_convo"%self.key)
+        text = current_topic["topic_text"]
+        options = self.generate_options(caller,mode=1)
         return text, options
         
     def endconvo(self, caller, raw_string, **kwargs):
